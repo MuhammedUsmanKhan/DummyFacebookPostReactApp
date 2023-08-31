07adb48ae529a275a04533347f23ca8e2b13c60f
MuhammedUsmanKhan <uktech0310@gmail.com>
2023-08-31 17:30:45 +0500

    dummy post created using react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const FacebookPost = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-white p-4 m-4 border rounded-md shadow-md">
+      {/* Post Header */}
+      <div className="flex items-center">
+        <img
+          src={props.profImage}
+          alt="User Avatar"
+          className="w-20  rounded-full mr-2"
+        />
+        <div>
+          <p className="font-semibold">{props.name}</p>
+          <p className="text-gray-600 text-sm">{props.time}</p>
+        </div>
+      </div>
+
+      {/* Post Content */}
+      <p className="my-4">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ac
+        urna eget justo vestibulum fermentum a nec purus. Fusce viverra nisl at
+        velit fringilla, in interdum felis bibendum. Sed eu justo id nibh
+        volutpat volutpat. Ut lacinia tellus nec justo eleifend, vel feugiat
+        arcu gravida.
+      </p>
+
+      {/* Post Image */}
+      <img
+        src={props.Image}
+        alt="Post Image"
+        className="w-full rounded-lg"
+      />
+
+      {/* Like, Comment, and Share */}
+      <div className="flex justify-between mt-4">
+        <div className="flex items-center space-x-4">
+          <div className="flex items-center">
+            <i className="far fa-thumbs-up text-blue-500"></i>
+            <p className="ml-2">Like</p>
+          </div>
+          <div className="flex items-center">
+            <i className="far fa-comment text-gray-600"></i>
+            <p className="ml-2">Comment</p>
+          </div>
+          <div className="flex items-center">
+            <i className="far fa-share-square text-gray-600"></i>
+            <p className="ml-2">Share</p>
+          </div>
+        </div>
+        <div className="flex items-center">
+          <i className="far fa-bookmark text-gray-600"></i>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+export default FacebookPost;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import FacebookPost from './App';
+import img from './image/my img.jpg'
+import postimg from './image/373016035_842820577210394_8933836662957754288_n.jpg'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <FacebookPost profImage = {img} Image = {postimg} name='Muhammed Usman Khan' time = '30 minutes ago' />
+    <FacebookPost profImage = {img} Image = {postimg} name='Muhammed Usman Khan' time = '30 minutes ago' />
+    <FacebookPost profImage = {img} Image = {postimg} name='Muhammed Usman Khan' time = '30 minutes ago' />
+    <FacebookPost profImage = {img} Image = {postimg} name='Muhammed Usman Khan' time = '30 minutes ago' />
+    <FacebookPost profImage = {img} Image = {postimg} name='Muhammed Usman Khan' time = '30 minutes ago' />
+  </React.StrictMode> 
 );
 
 // If you want to start measuring performance in your app, pass a function
